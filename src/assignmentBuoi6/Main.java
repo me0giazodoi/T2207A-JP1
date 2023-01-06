b9837b95533f871cdedbc08f45b514d2050e0f50
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-01-06 17:07:04 +0700

    Bài tập buổi 6

--- src/assignmentBuoi6/Main.java
+++ src/assignmentBuoi6/Main.java
@@ -1,7 +1,18 @@
 package assignmentBuoi6;
 
+import session3.Car;
+
 public class Main {
     public static void main(String[] args){
-
+        PhoneNumber[] pn = new PhoneNumber[5];
+        for (int i=0;i< pn.length;i++){
+            pn[0] = new PhoneNumber("Nguyễn Văn A",123456789);
+            pn[1] = new PhoneNumber("Nguyễn Văn B",987654321);
+            pn[2] = new PhoneNumber("Nguyễn Văn C",123789456);
+            pn[3] = new PhoneNumber("Nguyễn Văn D",456789123);
+            pn[4] = new PhoneNumber("Nguyễn Văn E",135792468);
+            System.out.println("Họ và tên: "+pn[i].getName());
+            System.out.println("Số điện thoại: "+pn[i].getPhone());
+        }
     }
 }
