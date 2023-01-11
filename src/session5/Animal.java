218472b13021654153f1390ffe3e11c091224eef
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-01-12 04:09:49 +0700

    update

--- src/session5/Animal.java
+++ src/session5/Animal.java
@@ -4,6 +4,9 @@ public class Animal {
     public String kind;
     protected  String color;
     public void eatMeat(){
-
+        System.out.println("Eatting...");
+    }
+    public void run(){
+        System.out.println("Running...");
     }
 }
--- src/session5/Main.java
+++ src/session5/Main.java
@@ -1,5 +1,20 @@
 package session5;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.color = "red";
+        a.eatMeat();
 
+        Tiger t = new Tiger();
+        t.color = "orange";
+        t.eatMeat();
+        t.run();
+        t.eatMeat();
+        t.eatMeat("Hello");
+        t.eatMeat("abc", 15);
+
+        Cat c = new Cat();
+        c.run();
+    }
 }
